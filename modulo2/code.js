18a165916194a463a75bd7cf48295e9f19a6fc8f
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2026-07-15 11:21:17 -0500

    add: modulo2 folder added

---
+++ modulo2/code.js
@@ -0,0 +1,22 @@
+const task = document.getElementById("task")
+const btn = document.getElementById("task_btn")
+const form = document.getElementById("form_task")
+
+let form2 = document.getElementsByClassName("form_tasks")
+
+console.log(form)
+console.log(form2)
+
+
+
+// form.addEventListener("submit",(event)=>{
+//     event.preventDefault()
+//     alert("submited")
+// })
+
+// btn.addEventListener("click",()=>{
+//     alert(task)
+// })
+
+
+
