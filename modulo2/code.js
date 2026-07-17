30f0d97d08bc9290907290a16733a18ac1437a67
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2026-07-16 21:49:51 -0500

    feat: add js functions to todo list

--- modulo2/code.js
+++ modulo2/code.js
@@ -1,22 +1,47 @@
 const task = document.getElementById("task")
 const btn = document.getElementById("task_btn")
 const form = document.getElementById("form_task")
+const inputText = document.getElementById("task")
+const taskList = document.getElementById("tasks_list")
+let tasks = [
+]
 
-let form2 = document.getElementsByClassName("form_tasks")
+function agregarTarea(tarea){
+    const index = tasks.length
+    tasks.push({
+        id: index,
+        tarea,
+        estado: false
+    })
+}
 
-console.log(form)
-console.log(form2)
+function mostrarTareas(tareas){
+    taskList.innerHTML = ""
+    tareas.forEach((tarea) => {
+        taskList.innerHTML +=`
+        <li class="task_item">
+            <input type="checkbox"">${tarea.tarea}</input>
+            <button class="dlt-btn" index=${tarea.id} value=${tarea.id}>X</button>
+        </li>`
+    })
+}
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    if(inputText.value.length > 0){
+        agregarTarea(inputText.value)
+        mostrarTareas(tasks)
+        inputText.value = ""
+        return
+    }
+    alert("Please fill the form")
+})
+
+document.addEventListener("click",(event) => {
+    console.log(event.target)
+})
 
 
-
-// form.addEventListener("submit",(event)=>{
-//     event.preventDefault()
-//     alert("submited")
-// })
-
-// btn.addEventListener("click",()=>{
-//     alert(task)
-// })
 
 
 
